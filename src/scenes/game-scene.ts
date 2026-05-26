a4fa37d5fa5e43829ab39f05c9725df93f648cd7
禾明(omnif) <543369008@qq.com>
2026-05-26 22:37:32 +0800

    fix: 监听键盘操作控制玩家

--- src/scenes/game-scene.ts
+++ src/scenes/game-scene.ts
@@ -3,12 +3,19 @@ import { SCENE_KEYS } from "../common/scene-keys.ts";
 import { ASSET_KEYS } from "../common/assets.ts";
 
 export class GameScene extends Phaser.Scene {
+  #cursorKeys!: Phaser.Types.Input.Keyboard.CursorKeys;
+  #player!: Phaser.GameObjects.Image;
+  #playerSpeed!: number;
+
   constructor() {
     super({
       key: SCENE_KEYS.GAME_SCENE,
     });
   }
 
+  init() {
+    this.#playerSpeed = 500;
+  }
   /**
    * @public
    * Tied to the Phaser Scene lifecycle. Will run one time after the PRELOAD
@@ -21,7 +28,34 @@ export class GameScene extends Phaser.Scene {
 
     // 添加游戏背景和其他元素
     this.add.image(width / 2, height / 2, ASSET_KEYS.BACKGROUND);
-    this.add.image(width / 2, height, ASSET_KEYS.JAR);
-    this.add.image(width / 2, height / 2, ASSET_KEYS.OBJECTS, "button2.png").setScale(0.75);
+    this.#player = this.add.image(width / 2, height, ASSET_KEYS.JAR);
+    this.add
+      .image(width / 2, height / 2, ASSET_KEYS.OBJECTS, "button2.png")
+      .setScale(0.75);
+
+    const keyboard = this.input.keyboard;
+    if (!keyboard) {
+      return;
+    }
+
+    this.#cursorKeys = keyboard.createCursorKeys();
+  }
+
+  update(time: number, delta: number) {
+
+    const moveStep = this.#playerSpeed * (delta / 1000);
+
+    if (this.#cursorKeys.left.isDown) {
+      this.#player.x -= moveStep;
+    } else if (this.#cursorKeys.right.isDown) {
+      this.#player.x += moveStep;
+    }
+
+    if(this.#player.x- this.#player.width/2 < 0) {
+      this.#player.x = this.#player.width/2;
+    }else if(this.#player.x + this.#player.width/2 > this.scale.width) {
+      this.#player.x = this.scale.width - this.#player.width/2;
+    }
+
   }
 }
